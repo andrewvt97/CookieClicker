aff070df37e29ef535634039b91a68ad11861d20
andrewvt97 <andrewvt97@gmail.com>
2025-04-30 21:16:59 -0400

    Click cookies

--- app/src/main/java/com/example/cookieclicker/MainActivity.kt
+++ app/src/main/java/com/example/cookieclicker/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.example.cookieclicker
 
+import android.content.ActivityNotFoundException
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -23,6 +28,10 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Share
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -31,6 +40,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -43,13 +53,17 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat
 import com.example.cookieclicker.data.Datasource
 import com.example.cookieclicker.model.Cookie
 import com.example.cookieclicker.ui.theme.CookieClickerTheme
 
+
+private const val TAG = "Main Activity"
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "OnCreate Called")
         enableEdgeToEdge()
         setContent {
             CookieClickerTheme {
@@ -62,6 +76,35 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "OnStart Called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume Called")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onRestart Called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause Called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop Called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy Called")
     }
 }
 
@@ -86,13 +129,35 @@ private fun CookieClickerApp(
     cookies: List<Cookie>
 ) {
 
-    var revenue by remember { mutableStateOf(0) }
-    var cookiesSold by remember { mutableStateOf(0) }
-    val currentCookieIndex by remember { mutableStateOf(0) }
-    var currentCookiePrice by remember { mutableStateOf(cookies[currentCookieIndex].price) }
-    var currentCookieImageId by remember { mutableStateOf(cookies[currentCookieIndex].imageId) }
+    var revenue by rememberSaveable { mutableStateOf(0) }
+    var cookiesSold by rememberSaveable { mutableStateOf(0) }
+    val currentCookieIndex by rememberSaveable { mutableStateOf(0) }
+    var currentCookiePrice by rememberSaveable { mutableStateOf(cookies[currentCookieIndex].price) }
+    var currentCookieImageId by rememberSaveable { mutableStateOf(cookies[currentCookieIndex].imageId) }
 
-    Scaffold() { contentPadding ->
+
+    Scaffold(topBar = {
+        val intentContext = LocalContext.current
+        val layoutDirection = LocalLayoutDirection.current
+        CookieClickerAppBar(
+            onShareButtonClicked = {
+                shareSoldCookiesInformation(
+                    intentContext = intentContext,
+                    cookiesSold = cookiesSold,
+                    revenue = revenue
+                )
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(
+                    start = WindowInsets.safeDrawing.asPaddingValues()
+                        .calculateStartPadding(layoutDirection),
+                    end = WindowInsets.safeDrawing.asPaddingValues()
+                        .calculateEndPadding(layoutDirection)
+                )
+                .background(MaterialTheme.colorScheme.primary)
+        )
+    }) { contentPadding ->
         DessertClickerScreen(
             revenue = revenue,
             cookiesClicked = cookiesSold,
@@ -110,6 +175,61 @@ private fun CookieClickerApp(
             },
             modifier = Modifier.padding(contentPadding)
         )
+    }
+}
+
+@Composable
+private fun CookieClickerAppBar(
+    onShareButtonClicked: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier,
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Text(
+            text = stringResource(R.string.app_name),
+            modifier = Modifier.padding(start = 16.dp),
+            color = MaterialTheme.colorScheme.onPrimary,
+            style = MaterialTheme.typography.titleLarge,
+        )
+
+        IconButton(
+            onClick = onShareButtonClicked,
+            modifier = Modifier.padding(end = 16.dp)
+        ) {
+            Icon(
+                imageVector = Icons.Filled.Share,
+                contentDescription = stringResource(R.string.share),
+                tint = MaterialTheme.colorScheme.onPrimary
+            )
+        }
+
+    }
+}
+
+
+fun shareSoldCookiesInformation(intentContext: Context, cookiesSold: Int, revenue: Int) {
+    val sendIntent = Intent().apply {
+        action = Intent.ACTION_SEND
+        putExtra(
+            Intent.EXTRA_TEXT,
+            intentContext.getString(R.string.share_text, cookiesSold, revenue)
+        )
+        type = "text/plain"
+    }
+
+    val shareIntent = Intent.createChooser(sendIntent, null)
+
+    try {
+        ContextCompat.startActivity(intentContext, shareIntent, null)
+    } catch (e: ActivityNotFoundException) {
+        Toast.makeText(
+            intentContext,
+            intentContext.getString(R.string.sharing_not_available),
+            Toast.LENGTH_LONG
+        ).show()
     }
 }
 
